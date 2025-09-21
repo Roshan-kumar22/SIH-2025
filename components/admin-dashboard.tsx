7dce5472ad13fbc53a4087e93f8cf7901f24be52
Roshan kumar <roshankumar@Roshans-MacBook-Air.local>
2025-09-22 04:08:38 +0530

    admin-dashboard

--- components/admin-dashboard.tsx
+++ components/admin-dashboard.tsx
@@ -123,7 +123,7 @@ export function AdminDashboard() {
                 </CardContent>
               </Card>
 
-              <Card>
+              <Card style={{cursor: "pointer"}} onClick={() => window.location.href = 'http://localhost:8080/dashboard'}>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle className="text-sm font-medium">Active Tourists</CardTitle>
                   <UserCheck className="h-4 w-4 text-muted-foreground" />
